dd774ba823b8771c03d4802fe0565867832ed6db
Philip Winston <pwinston@gmail.com>
2020-06-19 16:13:48 -0400

    delayed stack

--- nap.py
+++ nap.py
@@ -8,25 +8,45 @@ Small launcher script for napari for development/testing/debug:
 """
 import os
 import sys
+import time
 
+import dask
+import dask.array as da
 import numpy as np
+
+from text_image import create_text_image
 
 
 DATASETS = {}
 
-ENV = {
-    "NAPARI_PERFMON": "1",
-}
+ENV = {"NAPARI_PERFMON": "1"}
 
 
-def usage():
-    datasets = DATASETS.keys()
-    datasets_str = ", ".join(datasets)
-    print("USAGE: nap.py <dataset>")
-    print(f"Datasets: {datasets_str}")
+def _dump_env():
+    for key, value in os.environ.items():
+        if key.startswith("NAPARI"):
+            print(key, value)
 
 
-def run_napari():
+def run_napari(usage=False):
+    def delayed_stack():
+        @dask.delayed
+        def image(x):
+            time.sleep(1)
+            return np.array(create_text_image(x))
+
+        images = [
+            da.from_delayed(image(x), (1024, 1024), dtype=float)
+            for x in range(20)
+        ]
+        data = np.stack(images, axis=0)
+        return napari.view_image(data, name='delayed (1 second)')
+
+    def numbered():
+        images = [np.array(create_text_image(x)) for x in range(20)]
+        data = np.stack(images, axis=0)
+        return napari.view_image(data, name='numbered slices')
+
     def noise():
         return napari.view_image(
             np.random.random((5, 1024, 1024)), name='five 1k images'
@@ -54,6 +74,13 @@ def run_napari():
             name='big 3D timeseries',
         )
 
+    def small3d():
+        return napari.view_image(
+            np.random.random((3, 64, 64, 64)),
+            ndisplay=3,
+            name='small 3D timeseries',
+        )
+
     def labels():
         return napari.view_labels(
             np.random.randint(10, size=(20, 2048, 2048)),
@@ -61,16 +88,23 @@ def run_napari():
         )
 
     DATASETS = {
+        "delayed_stack": delayed_stack,
+        "numbered": numbered,
         "noise": noise,
         "big8": big8,
         "big16": big16,
-        "2d": big2d,
-        "3d": big3d,
+        "big2d": big2d,
+        "big3d": big3d,
+        "small3d": small3d,
         "labels": labels,
         "remote": "https://s3.embassy.ebi.ac.uk/idr/zarr/v0.1/4495402.zarr",
         "big": "/data-ext/4495402.zarr",
         "small": "/data-local/6001240.zarr",
     }
+
+    if usage:
+        print('\n'.join(DATASETS.keys()))
+        return 2
 
     name = sys.argv[1]
     data_set = DATASETS[name]
@@ -98,8 +132,9 @@ def run_napari():
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-        usage()
+        run_napari(usage=True)
         sys.exit(1)
 
+    _dump_env()
     os.environ.update(ENV)
     run_napari()
