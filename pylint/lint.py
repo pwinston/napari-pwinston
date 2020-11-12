c2fb2a01cfdcd852c29befcaf5c7bcacdb0cde4d
Philip Winston <pwinston@gmail.com>
2020-11-12 13:51:14 -0500

    Use click

--- pylint/lint.py
+++ pylint/lint.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """lint.py
 """
+import click
+
 from pylint.lint import Run
 from pathlib import Path
 
@@ -9,15 +11,17 @@ ROOT = Path("/Users/pbw/dev/napari")
 PATHS = [
     "napari/_vispy/experimental/*.py",
     "napari/layers/image/experimental/*.py",
+    "napari/_qt/experimental/*.py",
+    "napari/_qt/experimental/render/*.py",
 ]
 
 
 # Run(['--errors-only', 'myfile.py'])
 
 
-def _run_lint(paths):
+def _run_pylint(paths):
     str_paths = [str(path) for path in paths]
-    Run(str_paths)
+    Run(str_paths, do_exit=False)
 
 
 def _split_path_pattern(path):
@@ -25,17 +29,21 @@ def _split_path_pattern(path):
     return ROOT / directory, '*' + pattern
 
 
-def main():
-    paths = []
-
+@click.command()
+@click.argument('match_string', required=False)
+def lint(match_string):
     for spec in PATHS:
         dir_path, pattern = _split_path_pattern(spec)
 
-        print(f"Searching {dir_path} for {pattern}")
-        paths.extend(Path(dir_path).glob(pattern))
+        paths = Path(dir_path).glob(pattern)
 
-    _run_lint(paths)
+        if match_string is not None:
+            paths = [path for path in paths if match_string in str(path)]
+
+        if paths:
+            print(f"====== LINTING: {dir_path}/{pattern}")
+            _run_pylint(paths)
 
 
 if __name__ == '__main__':
-    main()
+    lint()
